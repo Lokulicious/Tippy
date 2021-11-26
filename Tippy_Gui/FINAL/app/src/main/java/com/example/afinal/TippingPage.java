d134b4d1415b5a692205e8da17b58d7ecc50df1f
Nick Bohnsack <nick.bohnsack@gmail.com>
2021-11-26 10:13:49 +0100

    added the gui mischa created to the git

---
+++ Tippy_Gui/FINAL/app/src/main/java/com/example/afinal/TippingPage.java
@@ -0,0 +1,36 @@
+package com.example.afinal;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TippingPage extends Fragment {
+
+    private TippingPageViewModel mViewModel;
+
+    public static TippingPage newInstance() {
+        return new TippingPage();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tipping_page_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(TippingPageViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Tippy_Gui/FINAL/app/src/main/java/com/example/afinal/TippingPageViewModel.java
@@ -0,0 +1,7 @@
+package com.example.afinal;
+
+import androidx.lifecycle.ViewModel;
+
+public class TippingPageViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
